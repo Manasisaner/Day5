1077a9fc3d18954781fef0265c69e2a89d4e177e
snehasonwane <snehasonwane01@gmail.com>
2022-04-29 22:19:43 +0530

    check leap year or not

--- Flipcoin/Flipcoin/Program.cs
+++ Flipcoin/Flipcoin/Program.cs
@@ -1,36 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-namespace FlippingCoin
+//namespace FlippingCoin
+//{
+//    class Program
+//{
+//    static void Main (string[] args)
+//    {
+//        Console.WriteLine("Let flip a coin 1000 times");
+//        Coinflip();
+//    }
+//    private static int Coinflip()
+//    {
+//        Random random = new Random();
+//        int heads = 0;
+//        int tails = 0;
+//        int result = 0;
+//        for (int i = 0; i<1000; i++)
+//        {
+//            result = random.Next(0, 2);
+//            if (result == 1)
+//            {
+//                heads++;
+//            }
+//            else
+//            {
+//                tails++;
+//            }
+
+//        }
+//        Console.WriteLine("heads was flipped {0} times", heads);
+//        Console.WriteLine("tails was flipped {0} times", tails);
+//        return result;
+//    }
+//}
+//}using System;  
+namespace CheckLeapYear
 {
     class Program
-{
-    static void Main (string[] args)
     {
-        Console.WriteLine("Let flip a coin 1000 times");
-        Coinflip();
-    }
-    private static int Coinflip()
-    {
-        Random random = new Random();
-        int heads = 0;
-        int tails = 0;
-        int result = 0;
-        for (int i = 0; i<1000; i++)
+        static void Main(string[] args)
         {
-            result = random.Next(0, 2);
-            if (result == 1)
-            {
-                heads++;
-            }
-            else
-            {
-                tails++;
-            }
-
+            Console.WriteLine("Enter Year : ");
+            int Year = int.Parse(Console.ReadLine());
+            if (Year % 4 == 0) 
+                Console.WriteLine("{0} is a Leap Year.", Year);
+            else Console.WriteLine("{0} is not a Leap Year.", Year);
+            Console.ReadLine();
         }
-        Console.WriteLine("heads was flipped {0} times", heads);
-        Console.WriteLine("tails was flipped {0} times", tails);
-        return result;
     }
 }
-}
+
